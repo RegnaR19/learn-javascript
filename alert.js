2541d77f63e1e2199c62a22a2c8497e5624ebff5
RegnaR <nar200@yandex.ru>
2022-12-23 14:12:43 +0500

    New Project

---
+++ alert.js
@@ -0,0 +1,24 @@
+let a = "Вадим";
+
+document.write(`Здравствуйте, ${a}. 
+Мы в ${new Date().getFullYear()} году`)
+
+console.log(`Здравствуйте, ${a}.  
+Мы в ${new Date().getFullYear()} году`);
+
+let message = 'Hello (yopta)'
+
+document.write(`<br>${message}<br>`);
+
+let userName = "Вадим"
+let age = 27
+
+document.write("Меня зовут ", userName, ", возраст ", age)
+
+let e = typeof(1)
+
+document.write(`<br>Тип: <b>${e}</b>`)
+
+let result = prompt('Ваше имя')
+
+alert(result)
